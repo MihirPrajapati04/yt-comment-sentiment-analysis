2014a0663be9513bdb200a86be42d4cc833b1d8b
MihirPrajapati04 <prajapatimihir685@gmail.com>
2025-03-27 22:00:01 +0530

    folder structure created using CookieCutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='End to End ML Project with the use of best MLOPS Practices which focuses on analyzing the youtube comments throgh a chrome plugin.',
+    author='Mihir Prajapati',
+    license='MIT',
+)
